5a1560ebe1772b787cced6951a64ffaf5ac04579
Belabdelli Amine <amine.belabdelli@univ-temouchent.edu.dz>
2025-03-22 16:58:38 +0000

    Add files via upload

---
+++ كلمة السر.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  2 22:26:10 2025
+
+@author: AmineBiblio
+"""
+
+pass_word =  int (input("enter your pass_word:\n"))
+if pass_word == 123:
+       print("welcome")
+else:
+       print('you are roung')
+                
